0f154ff0d72e99b282bf4cc03e69c5049f1c6c04
Aditya Firoda <adityafiroda@gmail.com>
2021-05-20 00:26:41 +0530

    Added Spring Config. Closes #22

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -16,14 +16,15 @@ import java.time.LocalDate;
 @Component
 public class DataLoader implements CommandLineRunner {
 
+
     private final OwnerService ownerService;
     private final VetService vetService;
 
-
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
